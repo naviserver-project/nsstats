2eab36d401fe53ed9268ad04bc1fd7032662ae53
Gustaf Neumann <neumann@wu-wien.ac.at>
2020-01-23 22:03:25 +0100

    support sub-second granularity of timestamps in [ns_info scheduled]

--- nsstats.tcl
+++ nsstats.tcl
@@ -1212,10 +1212,10 @@ proc _ns_stats.sched {} {
         set id          [lindex $s 0]
         set state       [lindex $s 1]
         set flags       [join [_ns_stats.getSchedFlagTypes [lindex $s 4]] "<br>"]
-        set next        [_ns_stats.fmtTime [lindex $s 9]]
-        set lastqueue   [_ns_stats.fmtTime [lindex $s 5]]
-        set laststart   [_ns_stats.fmtTime [lindex $s 6]]
-        set lastend     [_ns_stats.fmtTime [lindex $s 7]]
+        set next        [_ns_stats.fmtTime [expr {int([lindex $s 9])}]]
+        set lastqueue   [_ns_stats.fmtTime [expr {int([lindex $s 5])}]]
+        set laststart   [_ns_stats.fmtTime [expr {int([lindex $s 6])}]]
+        set lastend     [_ns_stats.fmtTime [expr {int([lindex $s 7])}]]
         set proc        [lindex $s 2]
         set arg         [lindex $s 3]
         set duration    [_ns_stats.fmtSeconds [lindex $s 8]]
@@ -1523,8 +1523,14 @@ proc _ns_stats.getSchedFlagTypes {flags} {
 }
 
 proc _ns_stats.fmtSeconds {seconds} {
+    if {$seconds == 0} {
+        return 0s
+    }
+    if {$seconds < 1} {
+        return [format %.2f [expr {$seconds * 1000}]]ms
+    }
     if {$seconds < 60} {
-        return "${seconds}s"
+        return "[format %.2f ${seconds}]s"
     }
 
     if {$seconds < 3600} {
